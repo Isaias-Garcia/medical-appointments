a8bdface9a11ff258939cc3640beabd51294e15b
Isaias-Garcia <isaiasgarciacuya.com>
2025-08-27 10:07:06 -0500

    refactor: :recycle: Simular creación de registro en base de datos RDS.

--- src/infrastructure/datasources/appointment-pe.datasource.impl.ts
+++ src/infrastructure/datasources/appointment-pe.datasource.impl.ts
@@ -3,6 +3,12 @@ import { AppointmentPEDatasource, CreateAppointmentPEDto } from "../../domain";
 
 export class AppointmentPEDatasourceImpl implements AppointmentPEDatasource {
   async create(createAppointmentPEDto: CreateAppointmentPEDto): Promise<void> {
+    if (process.env.SIMULATE_RDS) {
+      console.log("Data that would be stored in RDS MySQL:");
+      console.log(JSON.stringify(createAppointmentPEDto, null, 2));
+      return;
+    }
+
     await prismaPE.appointmentPE.create({
       data: createAppointmentPEDto!,
     });
